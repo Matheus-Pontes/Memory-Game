96992326944f9fedd9870c89344c331d23fa3c15
Matheus-Pontes <pontesm10@outlook.com>
2023-01-23 20:58:40 -0300

    Ajustando página inicial para ter apenas o nome do usuário

--- src/js/index.js
+++ src/js/index.js
@@ -1,154 +1,28 @@
-const $modalRegister = document.querySelector('#modalRegister');
-const $modalForgetPass = document.querySelector('#modalForgetPass');
-const $hyperlinkElements = document.getElementsByTagName('a');
-
-const toogleActiveModal = modal => modal.classList.toggle('active');
-
-const functionsHyperlink = {
-    'register': () => toogleActiveModal($modalRegister),
-    'btn-cancel': () => toogleActiveModal($modalRegister),
-    'forgetPassword': () => toogleActiveModal($modalForgetPass),
-    'btn2-cancel': () => toogleActiveModal($modalForgetPass),
-}
-
-for(let i=0; i < $hyperlinkElements.length; i++) {
-    $hyperlinkElements[i].addEventListener('click', () => {
-        if(functionsHyperlink[$hyperlinkElements[i].id]){
-            functionsHyperlink[$hyperlinkElements[i].id]();
-        }
-    });
-}
-
-function cleanField($field) {
-    $field.value = '';
-}
 
 // Principal form [user, password, button-Game]
 const Form = {
     user: document.querySelector('#user'),
-    password: document.querySelector('#password'),
 
     getValues() {
         return {
             user: Form.user.value,
-            password: Form.password.value
         }
     },
-
-    validateFields() {
-        const { user, password } = Form.getValues();
-
-        const { userName, passwordName } = Storage.get();
-
-        if(user.trim() === "" && password.trim() === ""){
-            throw new Error('Por favor, cadastre-se!');
-        } else if (user !== userName && password !== passwordName ){
-            throw new Error('Por favor, cadastre-se!')
-        } else if(user === userName && password === passwordName ){
-            window.open('./src/html/game.html', 'GAME')
-        }
-
-    }, 
     submit(event){
         event.preventDefault();
-
-        try {
-            Form.validateFields();
-        } catch (error) {
-            alert(error.message);
-        }
+        Storage.set();
+        window.open('./src/html/game.html', 'GAME')
+        this.user.value = "";
     }   
 }
-
-// form cadastro 
-// salve afther click
-// verify to login
-
-const Cadastro = {
-    user: document.querySelector('#name'),
-    password: document.querySelector('#pass'),
-
-    getValues() {
-        return {
-            user: Cadastro.user.value,
-            password: Cadastro.password.value,
-        }
-    },
-
-    validateFields(){
-        const { user, password } = Cadastro.getValues();
-
-        if(user.trim() === "" && password.trim() === ""){
-            return false;
-        } else {
-            Storage.set();
-            toogleActiveModal($modalRegister);
-            cleanField(Cadastro.user);
-            cleanField(Cadastro.password);
-            
-            return true;
-        }
-    },
-    
-    submit(event){
-        event.preventDefault();
-        
-        try {
-            let response = Cadastro.validateFields();
-            if(response)
-                alert('Operação realizada com sucesso.');
-            else 
-                alert('Informe usuário e senha.');
-
-        } catch (error) {
-            alert(error.message);
-        }
-    }   
-} 
 
 const Storage = {
     get(){
         return{
             userName: localStorage.getItem('user') || [],
-            passwordName: localStorage.getItem('password') || [],
         }
     },
     set(){
-        localStorage.setItem('user', Cadastro.user.value);
-        localStorage.setItem('password', Cadastro.password.value);
+        localStorage.setItem('user', Form.getValues().user);
     }
 }
-
-const ForgetPassword = {
-    newPassword: document.querySelector("#newPassword"),
-    repeatNewPassword: document.querySelector("#repeatNewPassword"),
-    
-    validateNewPass: function () {
-        
-        if (ForgetPassword.newPassword.value != ForgetPassword.repeatNewPassword.value) {
-            return false;
-        }
-        else {
-            localStorage.setItem('password', ForgetPassword.newPassword.value);
-            toogleActiveModal($modalForgetPass);
-            cleanField(ForgetPassword.newPassword);
-            cleanField(ForgetPassword.repeatNewPassword);
-            return true;
-        }
-    },
-    
-    submit: function(e) {
-        e.preventDefault();
-        
-        try {
-            let response = ForgetPassword.validateNewPass();
-            if(response)
-                alert('Operação realizada com sucesso.');
-            else 
-                alert("BAAAMMM !!! Atenção senhas diferentes !!! ");
-        }
-        catch(e) {
-            alert(e.message);
-        }
-    }
-};
